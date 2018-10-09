0fceb3911a62af6430284cc5629ebf45ac2dfb23
Gavin Aboulhosn <gavinaboulhosn@gavins-imac.attlocal.net>
2018-10-09 16:29:22 -0400

    initial commit

---
+++ tests/CPDTest.py
@@ -0,0 +1,12 @@
+import os
+import h5py
+from lib.CPD import *
+
+"""
+The purpose of this test is to find changepoints in waveforms captured by 
+oscilloscope.  This file will load the .mat files provided in the data
+directory of this folder.  The goal of using change-point detection is to
+minimize the amount of data we will be processing using Dynamic Time Warping.
+"""
+if __name__ == '__main__':
+    pass
