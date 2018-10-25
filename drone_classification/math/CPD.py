7d559e95dd0e25050965862bd84eddb4bff5d2f2
Gavin Aboulhosn <gavinaboulhosn97@gmail.com>
2018-10-25 16:08:52 -0400

    fix CPD test files

--- drone_classification/math/CPD.py
+++ drone_classification/math/CPD.py
@@ -1,5 +1,8 @@
 import matplotlib.pyplot as plt
 import ruptures as rpt
+from time import time
+from drone_classification.get_data_path import get_data_path
+from drone_classification.matio import matio
 
 # algo = rpt.Dynp(model="rbf").fit(y1c)              # dynamic programing O(k*n^2)
 # algo = rpt.Binseg(model="rbf").fit(y)              # binary segmentation O(nlogn)
@@ -17,5 +20,26 @@ def changepoint_detection(signal, bkps = 1, window_width = 100, display = False)
 
 
 
+
+"""
+The purpose of this test is to find changepoints in waveforms captured by 
+oscilloscope.  This file will load the .mat files provided in the data
+directory of this folder.  The goal of using change-point detection is to
+minimize the amount of data we will be processing using Dynamic Time Warping.
+"""
+
+
+if __name__ == '__main__':
+
+    path = get_data_path(4)
+    mat = matio(path)
+    data = mat.extract_data()
+    start_time = time()
+    bkps = changepoint_detection(data[::5], display=True)
+    end_time = time()
+    new_signal = data[bkps[0]*5:]
+    plt.plot(new_signal)
+    plt.show()
+    print(end_time-start_time)
 
 
--- tests/baseTest.py
+++
@@ -1,4 +0,0 @@
-from drone_classification.math.CPD import changepoint_detection
-from drone_classification.get_data_path import get_data_path
-from drone_classification.matio import matio
-import matplotlib.pyplot as plt
