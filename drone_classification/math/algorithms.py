be169d5f0ea3fb727c1ef0adab47124862916657
Gavin Aboulhosn <gavinaboulhosn97@gmail.com>
2018-11-05 16:23:42 -0500

    add function definition for wavelet decomp

--- drone_classification/math/algorithms.py
+++ drone_classification/math/algorithms.py
@@ -1,14 +1,30 @@
 import matplotlib.pyplot as plt
 import ruptures as rpt
-from time import time
+import pywt
 from fastdtw import fastdtw
 from scipy.spatial.distance import euclidean
 from drone_classification.get_data_path import get_data_path
 from drone_classification.matio import matio
 
-# algo = rpt.Dynp(model="rbf").fit(y1c)              # dynamic programing O(k*n^2)
-# algo = rpt.Binseg(model="rbf").fit(y)              # binary segmentation O(nlogn)
-# algo = rpt.Window(width = 100, model="rbf").fit(y)  # window-based O(n)
+
+
+
+
+class DTW:
+    """
+
+    """
+
+    def __init__(self, signalA, signalB, dist_measure=euclidean):
+        pass
+
+    def dtw(self, signal):
+        pass
+
+
+
+def wavelet_decomposition(signal, levels):
+    pass
 
 def changepoint_detection(signal, bkps = 1, window_width = 100, display = False):
     algo = rpt.Window(width=window_width, model="rbf").fit(signal)
@@ -19,13 +35,9 @@ def changepoint_detection(signal, bkps = 1, window_width = 100, display = False)
     return breakpoints
 
 
-class DTW(fastdtw):
-    """
 
-    """
 
-    def __init__(self, signalA, signalB, dist_measure=euclidean):
-        pass
+
 
 
 """
@@ -41,12 +53,9 @@ if __name__ == '__main__':
     path = get_data_path(4)
     mat = matio(path)
     data = mat.extract_data()
-    start_time = time()
     bkps = changepoint_detection(data[::5], display=True)
-    end_time = time()
     new_signal = data[bkps[0]*5:]
     plt.plot(new_signal)
     plt.show()
-    print(end_time-start_time)
 
 
